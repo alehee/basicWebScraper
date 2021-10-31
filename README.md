0104cd39566420bd52dc76ab5d74ee2955a95375
Aleksander Heese <36664250+alehee@users.noreply.github.com>
2021-10-31 21:21:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# basicWebScraper
+ C# .NET Core 3.1 basic web scraper program to understand fundamentals and scrap some sites!
---
+++ basicWebScraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace basicWebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
